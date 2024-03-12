511c9256bc266cfeb88de8f6c13e49ea213c5d19
Denowsky <denowsky26@yahoo.com>
2024-03-12 15:24:37 +0300

    Изменение admin

--- modelsAndForms/admin.py
+++ modelsAndForms/admin.py
@@ -54,6 +54,7 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name']
     fieldsets = [
         (
             'Категории',
